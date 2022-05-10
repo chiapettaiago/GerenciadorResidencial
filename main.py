f4301f3d2cb59a79a26a316be92f55f5d8ea5bfd
Iago Chiapetta <iagochiapetta@gmail.com>
2022-05-10 08:52:49 -0300

    Atualização 10 de maio de 2022
    Algoritmo de seleção de setor adicionado

--- main.py
+++ main.py
@@ -18,12 +18,45 @@ st.markdown(hide_menu_style, unsafe_allow_html=True)
 st.title("Gerenciador residencial e empresarial")
 
 st.sidebar.title("Opções")
-st.sidebar.selectbox("Cômodo ou setor a ser gerenciado", ["Sala", "Quarto", "Cozinha", "Banheiro", "Escritório"])
+comodo = st.sidebar.selectbox("Cômodo ou setor a ser gerenciado", ["Sala", "Quarto", "Cozinha", "Banheiro", "Escritório"])
 
-dataFaxina = st.date_input("Data da última faxina completa")
-hoje = datetime.date.today()
-quantidadeDias = abs((hoje - dataFaxina).days)
-if (quantidadeDias >= 3):
-    st.text("Você já precisa faxinar essa parte.")
-else:
-    st.text("Pode descansar. A faxina ainda não é necessária.")
+if (comodo == "Sala"):
+    dataFaxina = st.date_input("Data da última faxina completa da Sala")
+    hoje = datetime.date.today()
+    quantidadeDias = abs((hoje - dataFaxina).days)
+    if (quantidadeDias >= 7):
+        st.text("Você já precisa faxinar essa parte.")
+    else:
+        st.text("Pode descansar. A faxina ainda não é necessária.")
+elif (comodo == "Quarto"):
+    dataFaxina = st.date_input("Data da última faxina completa do Quarto")
+    hoje = datetime.date.today()
+    quantidadeDias = abs((hoje - dataFaxina).days)
+    if (quantidadeDias >= 3):
+        st.text("Você já precisa faxinar essa parte.")
+    else:
+        st.text("Pode descansar. A faxina ainda não é necessária.")
+elif (comodo == "Cozinha"):
+    dataFaxina = st.date_input("Data da última faxina completa na Cozinha")
+    hoje = datetime.date.today()
+    quantidadeDias = abs((hoje - dataFaxina).days)
+    if (quantidadeDias >= 5):
+        st.text("Você já precisa faxinar essa parte.")
+    else:
+        st.text("Pode descansar. A faxina ainda não é necessária.")
+elif (comodo == "Banheiro"):
+    dataFaxina = st.date_input("Data da última faxina completa do Banheiro")
+    hoje = datetime.date.today()
+    quantidadeDias = abs((hoje - dataFaxina).days)
+    if (quantidadeDias >= 3):
+        st.text("Você já precisa faxinar essa parte.")
+    else:
+        st.text("Pode descansar. A faxina ainda não é necessária.")
+elif (comodo == "Escritório"):
+    dataFaxina = st.date_input("Data da última faxina completa do Escritório")
+    hoje = datetime.date.today()
+    quantidadeDias = abs((hoje - dataFaxina).days)
+    if (quantidadeDias >= 7):
+        st.text("Você já precisa faxinar essa parte.")
+    else:
+        st.text("Pode descansar. A faxina ainda não é necessária.")
